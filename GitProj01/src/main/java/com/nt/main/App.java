8396f06bb9902125a4022bf5dec29c9655d438f4
mdarman6207570 <mdarman6207570@gmail.com>
2026-03-19 15:30:55 +0530

    tl commit.

--- GitProj01/src/main/java/com/nt/main/App.java
+++ GitProj01/src/main/java/com/nt/main/App.java
@@ -9,6 +9,7 @@ public class App {
     public static void main(String[] args) {
        App app = new App();
        System.out.println("it's me DEVIL from TL");
+       //how are u
        System.out.println("sum is  ::+"+app.sum(10,20));
     }
 }
