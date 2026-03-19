63bb4dbbf9deb4584da0394838c35f436700d91e
mdarman6207570 <mdarman6207570@gmail.com>
2026-03-19 15:51:56 +0530

    dev commit.

--- GitProj01/src/main/java/com/nt/main/App.java
+++ GitProj01/src/main/java/com/nt/main/App.java
@@ -8,13 +8,10 @@ public class App {
 	}
     public static void main(String[] args) {
        App app = new App();
-<<<<<<< HEAD
        System.out.println("Hello from dev1");
-=======
        System.out.println("it's me DEVIL from TL");
        //how are u
-       //mksd sixxer
->>>>>>> branch 'main' of https://github.com/mdarman6207570/ToolsFS-903.git
+       //mksd sixxerr
        System.out.println("sum is  ::+"+app.sum(10,20));
     }
 }
